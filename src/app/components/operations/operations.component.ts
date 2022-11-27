33f45edac4c5a3cfe1f291e37721693b78b3f10b
andfelsil98 <and_fel_sil1998@hotmail.com>
2022-11-27 13:45:31 -0500

    version 1.0.0

---
+++ src/app/components/operations/operations.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operations',
+  templateUrl: './operations.component.html',
+  styleUrls: ['./operations.component.css']
+})
+export class OperationsComponent {
+  optionSelected: string = '';
+  lbPrice: number = 0;
+  clientPrice: number = 0;
+  clientGrams: number = 0;
+  result: number = 0;
+  grams = 500;
+  porcentaje = 0;
+  unidad = '';
+
+  borrarInputRadio() {
+    this.optionSelected = '';
+    this.clientGrams = 0;
+    this.clientPrice = 0;
+    this.lbPrice = 0;
+    this.result = 0;
+  }
+  borrarInputText() {
+    this.clientGrams = 0;
+    this.clientPrice = 0;
+    this.lbPrice = 0;
+    this.result = 0;
+  }
+
+  calculateGrams() {
+    if (this.clientPrice == 0 || this.lbPrice == 0) {
+      return window.alert('Por favor introdusca un valor mayor a 0');
+    } else {
+      this.result = (this.clientPrice * this.grams) / this.lbPrice;
+      this.unidad = 'gramos';
+    }
+  }
+
+  calculatePrice() {
+    if (this.clientGrams == 0 || this.lbPrice == 0) {
+      return window.alert('Por favor introdusca un valor mayor a 0');
+    } else {
+      this.result = (this.clientGrams * this.lbPrice) / this.grams;
+      this.porcentaje = (this.clientGrams * 100) / this.lbPrice;
+      this.unidad = 'pesos';
+    }
+  }
+}
